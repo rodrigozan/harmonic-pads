81bb65123f0adce902727fa2d4fccc91f6a85069
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2025-05-07 11:13:27 -0300

    👆 Updated Header Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="text-center mb-4">
+      <h1 className="h3 mb-2">Harmonic Pad</h1>
+      <p className="small mb-0">Continuous musical tones for meditation, focus and creativity</p>
+    </div>
+  )
+}
+
+export default Header
